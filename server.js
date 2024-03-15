3ca9716d5b2e8abe3740b6bd24e95cab64ff86ff
lalit <kumarlalit94111@gmail.com>
2024-03-15 23:37:53 +0530

    Updated PORT

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const io = require('socket.io')(server, {
   cors: {
     // origin: process.env.CLIENT_URL,
     // origin: 'http://localhost:3000',
-    origin: "https://mern-chat-app-frontend-alpha.vercel.app",
+    origin: "*",
     methods: ['GET', 'POST']
   }
 })
